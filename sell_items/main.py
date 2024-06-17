de8cbdf259831d7fd251d6af91edc4878eccfc6d
ppabis <piotr@pabis.eu>
2024-06-17 18:38:33 +0200

    lambda handler

--- sell_items/main.py
+++ sell_items/main.py
@@ -1,31 +1,24 @@
-from actions import get_inventory, sell_item
+from actions import sell_items
 from auth import get_headers
-import os
+import os, json
 
 HEADERS = get_headers()
 # This is the amount of each item that will be left and not sold.
-ITEMS_TO_LEAVE = int(os.getenv('ITEMS_TO_LEAVE', "25"))
+ITEMS_TO_LEAVE = int(os.getenv('ITEMS_TO_LEAVE', "150"))
 
 def lambda_handler(event, context):
-    items, gold = get_inventory()
+    if event is str:
+        event: dict = json.loads(event)
+    
+    try:
+        gp = sell_items(HEADERS, ITEMS_TO_LEAVE, event)
+    except Exception as e:
+        return {
+            "statusCode": 400,
+            "body": str(e)
+        }
+    
     return {
         "statusCode": 200,
-        "body": items
+        "body": "Earned {gp:.2f} gold."
     }
-
-if __name__ == "__main__":
-    from pprint import pprint
-    
-    items, gp = get_inventory(HEADERS)
-    food = items['food']
-    
-    print(f">> Current gold {gp:.2f}. Food:")
-    pprint(food)
-
-    if food['CottonCandyBlue'] > 30:
-        sold = sell_item(HEADERS, 'food', 'CottonCandyBlue', 2)
-    
-    items, gp = get_inventory(HEADERS)
-    food = items['food']
-    print(f">> After sale, gold: {gp:.2f}. Food:")
-    pprint(food)
