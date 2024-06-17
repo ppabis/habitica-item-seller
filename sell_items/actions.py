84090a0e74be727711cacdac6bb16b9cb723ac34
ppabis <piotr@pabis.eu>
2024-06-17 18:38:23 +0200

    sell all items from category

--- sell_items/actions.py
+++ sell_items/actions.py
@@ -30,3 +30,55 @@ def sell_item(headers: dict, item_type: str, item_key: str, count: int):
     if code == 200:
         return response.json()
     raise Exception(response.json()['message'])
+
+
+def is_event_valid(event) -> bool:
+    """
+    Validates the event so that sell_category is present and correct.
+    """
+    return ('sell_category' in event)\
+          and (event['sell_category'] in ['food', 'eggs', 'hatchingPotions'])
+
+
+def sell_items(headers: dict, items_to_leave: int, event) -> float:
+    """
+    Sells items of a specific category in `event['sell_category']`.
+    We need to sell items only in the category specified to not sell too much at once.
+    """
+    if not is_event_valid(event):
+        print("No sell_category specified!")
+        raise ValueError("Missing 'sell_category' key in event. And such, ignoring this event.")
+    
+    category = event['sell_category']
+    
+    items, gold = get_inventory(headers)
+    print(f"Starting with gold [{category}] {gold:.2f}.")
+    items_to_sell = items[category]
+
+    # Filter items that we have more than threshold.
+    items_to_sell = {k: v for k, v in items_to_sell.items() if v > items_to_leave}
+    
+    # Print history of what is being done.
+    string_items = ", ".join([f"{k} ({v})" for k, v in items_to_sell.items()])
+    print(f"Items to sell [{category}]: {string_items}")
+    
+    last_sale = None
+    # Sell items.
+    for item, amount in items_to_sell.items():
+        count = amount - items_to_leave
+        print(f"Selling {count} of {item}.")
+        try:
+            _sold = sell_item(headers, event['sell_category'], item, count)
+        except Exception as e:
+            print(f"Failed to sell {item}: {e}.")
+            continue
+        last_sale = _sold
+        print(f"Sold {count} of {item}.")
+    
+    if last_sale is not None:
+        gp = last_sale['data']['stats']['gp']
+        print(f"Gold after sale [{category}]: {gp:.2f}. Made {gp - gold:.2f} gold.")
+    else:
+        print(f"Didn't sell anything for {category}.")
+
+    return gp - gold
